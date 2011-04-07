1f4de811381fb6441ec39bbed533b4140e9cbe64
barubary <barubary@64c19138-535a-11de-9335-abd5230c83eb>
2011-04-07 13:51:09 +0000

    C#: removed the BitConverter reference in the Huffman decompressor, and replaced the call with one to a local utility method (to be sure that the byte*->u32 conversion always uses the correct endianness.

---
+++ trunk/CSharp/DSDecmp/Utils/IOUtils.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DSDecmp
+{
+    /// <summary>
+    /// Class for I/O-related utility methods.
+    /// </summary>
+    public static class IOUtils
+    {
+        /// <summary>
+        /// Returns a 4-byte unsigned integer as used on the NDS converted from four bytes
+        /// at a specified position in a byte array.
+        /// </summary>
+        /// <param name="buffer">The source of the data.</param>
+        /// <param name="offset">The location of the data in the source.</param>
+        /// <returns>The indicated 4 bytes converted to uint</returns>
+        public static uint ToNDSu32(byte[] buffer, int offset)
+        {
+            return (uint)(buffer[offset]
+                        | (buffer[offset + 1] << 8)
+                        | (buffer[offset + 2] << 16)
+                        | (buffer[offset + 3] << 24));
+        }
+    }
+}
